56791e896fbaac471d156c42286aa156c24bc033
Suhel <jahidulsuhel@gmail.com>
2024-12-03 21:59:15 +0600

    JavaScript

---
+++ Callback_Promise_Async/index.js
@@ -0,0 +1,13 @@
+const prompt = require("prompt-sync")();
+// syncronous 
+// let a = prompt("what is your name?");
+// let b = prompt("what is your age?");
+// let c = prompt("what is your favorite color?")
+// console.log(a + " is " + b + " years old and has " + c + " favorite color")
+
+// Asyncronous
+// console.log("start")
+// setTimeout( function(){
+//     console.log("i am good")
+// }, 3000)
+// console.log("end")
