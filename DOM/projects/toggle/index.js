08c927dffca7f1403649c59059f010cf61d89db7
Jahidul Hasan Suhel <jahidulsuhel@gmail.com>
2026-02-18 14:37:37 +0600

    toggle project

---
+++ DOM/projects/toggle/index.js
@@ -0,0 +1,8 @@
+console.log("Toggle info project");
+
+function toggleInfo(){
+    console.log("toogling")
+    const para = document.getElementById("myParagraph");
+
+    para.classList.toggle("hidden")
+}
