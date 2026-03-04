c6644b91f96b7e214227813b0d83bd8269659fb9
Jahidul Hasan Suhel <jahidulsuhel@gmail.com>
2026-03-04 07:29:35 +0600

    fetch api

--- fetch/index.js
+++ fetch/index.js
@@ -78,3 +78,71 @@ const newPostData = {
 
 createPost(newPostData);
 
+// - 8. fetch() Usage: Updating an Entire Resource
+
+async function updatePost(postData) {
+    console.log("Replacing a POST Data...");
+    const API_URL =
+        "http://localhost:3000/posts/a9057951-515e-3da2-9793-af29719d0e33";
+    try {
+        const response = await fetch(API_URL, {
+            method: "PUT",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(postData),
+        });
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const updatePostData = {
+    title: "What is 40 Days of JavaScript?",
+    views: 160,
+};
+
+const updated = updatePost(updatePostData);
+console.log(updated);
+
+// - 9. fetch() Usage: Updating a Part of the Resource
+
+async function updateViews(viewData) {
+    console.log("Updating a View with Partial Data...");
+    const API_URL = `http://localhost:3000/posts/${viewData.id}`;
+    try {
+        const response = await fetch(API_URL, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(viewData),
+        });
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const updatedPart = updateViews({
+    id: "a9057951-515e-4da2-9793-af29719d0e33",
+    views: 100,
+});
+console.log(updatedPart);
+
+// - 10. fetch() Usage: Deleting Resource
+async function deletePost(postId) {
+    console.log("Deleting Post...");
+    const API_URL = `http://localhost:3000/posts/${postId}`;
+    try {
+        const response = await fetch(API_URL, {
+            method: "DELETE",
+        });
+        const result = await response.json();
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const deleted = deletePost("a9057951-515e-4da2-9793-af29719d0e33");
+
